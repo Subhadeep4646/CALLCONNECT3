5463e7afa456c2717cda4d41479717a170f152cf
Subhadeep <neurondey2017@gmail.com>
2025-07-25 00:48:42 +0530

    initial commit

---
+++ backend/src/routes/UserRoute.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const userMiddleware = require('../../middleware/usermiddleware');
+const router=express.Router();
+const {getRecommendedUsers,getMyFriends,sendFriendRequest,AcceptFriendRequest,getFriendRequest,getOutgoingFriendReqs}=require('../controllers/UserControl')
+
+router.use(userMiddleware);
+
+router.get("/RecommendedUsers",getRecommendedUsers);
+router.get("/friends",getMyFriends);
+router.post("/friend-request/:id",sendFriendRequest);
+router.put("/friend-request/:id/accept",AcceptFriendRequest);
+router.get("/friend-requests",getFriendRequest);
+router.get("/outgoing-friend-requests",getOutgoingFriendReqs);
+
+module.exports=router;
